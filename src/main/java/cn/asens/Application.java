b7bcc567a01222c65152494cd4c0207d9dceccfd
Asens <297941676@qq.com>
2019-01-17 12:10:10 +0800

    use SpringBoot default

---
+++ src/main/java/cn/asens/Application.java
@@ -0,0 +1,24 @@
+package cn.asens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Asens
+ * create 2019-01-17 12:04
+ **/
+@SpringBootApplication
+@RestController
+public class Application {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @RequestMapping("/")
+    public Object index(){
+        return "success";
+    }
+}
