6b503936a2704a9d210d5fa6e632a4ffb0c49c5a
297941676@qq.com <asens1314@163.com>
2019-07-24 09:44:05 +0800

    test build

--- src/test/java/cn/asens/LTest.java
+++ src/test/java/cn/asens/LTest.java
@@ -5,7 +5,24 @@ import org.junit.Test;
 
 public class LTest {
     @Test
-    public void testSayHello(){
-        System.out.println(1);
+    public void testSayHello() throws InterruptedException {
+        int success = 0;
+        int total = 10;
+        int between = 50;
+        String address = "https://www.baidu.com";
+
+        for (int i = 0; i < total; i++) {
+            String result = HttpUtil.get(address);
+            if(result!=null && !result.equals("success")){
+                success++;
+            }
+            Thread.sleep(between);
+            System.out.println("success/total:"+success+"/"+total);
+        }
+
+        System.out.println("================================");
+        System.out.println("total:"+total);
+        System.out.println("success:"+success);
+        System.out.println("================================");
     }
 }
