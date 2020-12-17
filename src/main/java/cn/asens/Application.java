2dee046540ee4d216e2fe38423babb8eed300eb9
297941676@qq.com <asens1314@163.com>
2020-12-17 15:27:54 +0800

    test build

--- src/main/java/cn/asens/Application.java
+++ src/main/java/cn/asens/Application.java
@@ -1,24 +1,23 @@
 package cn.asens;
 
+import cn.asens.controller.SampleController;
+import cn.asens.dao.UserDao;
+
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
- * @author Asens
- * create 2019-01-17 12:04
- **/
+ * Created by Asens on 2017/7/13
+ */
+
 @SpringBootApplication
-@RestController
+@EnableJpaRepositories(basePackages  = "cn.asens.dao")
 public class Application {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
-    }
-
-    @RequestMapping("/")
-    public Object index(){
-        return "success122354";
     }
 }
